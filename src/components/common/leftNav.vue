9ce71514325f402a969d8157e39865c002147e0f
lh971110 <2735681669@qq.com>
2018-08-14 13:01:11 +0800

    update

---
+++ src/components/common/leftNav.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="left-nav">
+        <ul>
+            <li class="icon iconfont icon-shouyin">
+                <div>收银</div>
+            </li>
+            <li class="icon iconfont icon-dianpucopy">
+               <div>店铺</div>
+            </li>
+            <li class="icon iconfont icon-hanbao">
+                <div>商品</div>
+            </li>
+            <li class="icon iconfont icon-huiyuanqia-mianxing">
+                <div>会员</div>
+            </li>
+            <li class="icon iconfont icon-tongji">
+                <div>统计</div>
+            </li>
+            <li class="icon iconfont icon-dianpucopy">
+                <div>设置</div>
+            </li>
+        </ul>
+
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+.left-nav{
+    color: #ffffff;
+    font-size: 10px;
+    height: 100%;
+    background-color: #1d8ce0;
+    float: left;
+    width: 5%;
+}
+.iconfont{
+    font-size: 14px;
+}
+.left-nav ul{
+    padding: 0px;
+    margin: 0px;
+}
+.left-nav ul li{
+  list-style: none;
+  text-align: center;
+  border-bottom: 1px solid #20a0ff;
+  padding:10px; 
+}
+
+</style>
+
+
